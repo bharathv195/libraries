df0932b53ef0c5636067584add8c98dfe6b3c769
bharathv195 <96901730+bharathv195@users.noreply.github.com>
2022-01-26 15:18:41 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def newMaven()
 {
   sh 'mvn package'
 }
+def newDeply(ip,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/DeclarativePipeline3/webapp/target/webapp.war ubuntu@${ip}:var/lib/tomcat9/webapps/${appname}.war"
+}
