d0abb4983abb3e4b4640930084572eefc261b95e
bharathv195 <96901730+bharathv195@users.noreply.github.com>
2024-04-12 15:21:17 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,9 @@ def newMaven()
 {
    sh "mvn clean package"
 }
+def newDepl(src,user,ip,appname)
+{
+   sshagent(['fff70456-64d3-4cda-80ba-e1682fe9ff90']) {
+                        sh "scp -o strictHostKeyChecking=No target/${src}.war ${user}@${ip}:/opt//apache-tomcat-9.0.87/webapps/${appname}.war"
+      }
+}
